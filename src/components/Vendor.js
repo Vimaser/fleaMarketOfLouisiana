6ffb09108a7bd325669acb99d829b25880564b3a
Vimaser <thfeske@gmail.com>
2024-01-16 14:30:37 -0600

    eslint2

--- src/components/Vendor.js
+++ src/components/Vendor.js
@@ -2,27 +2,25 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getFirestore, doc, getDoc, updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import useUserRole from "./useUserRole"; // Assuming you have this hook
+import useUserRole from "./useUserRole";
 import "../firebaseConfig";
 
 const Vendor = () => {
   const [vendor, setVendor] = useState(null);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
-  const [currentUser, setCurrentUser] = useState(null); // State for current user
+  const [currentUser, setCurrentUser] = useState(null);
   const [imageFile, setImageFile] = useState(null);
   const { vendorID } = useParams();
   const db = getFirestore();
-  const userRole = useUserRole(); // Get the current user's role
+  const userRole = useUserRole();
   const auth = getAuth();
 
-  // State hooks for editable fields
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [contactInformation, setContactInformation] = useState("");
   const [locationInMarket, setLocationInMarket] = useState("");
   const [productCategories, setProductCategories] = useState("");
-  //const [accessToken, setAccessToken] = useState(null);
   const [images, setImages] = useState("");
   const [avatar, setAvatar] = useState("");
   const [lotNum, setLotNum] = useState("");
@@ -50,53 +48,37 @@ const Vendor = () => {
     };
 
     fetchData();
-
     onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
     });
-
-    // OAuth callback handling
-    const urlParams = new URLSearchParams(window.location.hash.substring(1));
-    const token = urlParams.get("access_token");
-    const state = urlParams.get("state");
-
-    // Verify the 'state' parameter here if you sent one in the redirect
-    if (token) {
-      setAccessToken(token);
-      // You might want to redirect the user away from the callback URL here
-    }
   }, [vendorID, db, auth]);
 
   const redirectToImgurOAuth = () => {
     const clientId = process.env.REACT_APP_IMGUR_CLIENT_ID;
-    const redirectUri = encodeURIComponent("https://thefleamarketoflouisiana.com/auth-callback");
-    
-    // Store vendorID before redirecting
+    const redirectUri = encodeURIComponent(
+      "https://thefleamarketoflouisiana.com/auth-callback"
+    );
     localStorage.setItem("vendorID", vendorID);
-  
     window.location.href = `https://api.imgur.com/oauth2/authorize?client_id=${clientId}&response_type=token&redirect_uri=${redirectUri}`;
   };
-  
+
   const uploadImageToImgur = async (imageFile) => {
     const formData = new FormData();
     formData.append("image", imageFile);
-
-    // Retrieve the access token from local storage
     const accessToken = localStorage.getItem("access_token");
-    console.log("AccessToken:", accessToken)
 
     try {
       const response = await fetch("https://api.imgur.com/3/image", {
         method: "POST",
         headers: {
-          Authorization: `Bearer ${accessToken}`, // Use the access token here
+          Authorization: `Bearer ${accessToken}`,
         },
         body: formData,
       });
 
       const data = await response.json();
       if (data.success) {
-        return data.data.link; // URL of the uploaded image
+        return data.data.link;
       } else {
         throw new Error("Failed to upload image");
       }
@@ -110,7 +92,7 @@ const Vendor = () => {
     if (imageFile) {
       const uploadedImageUrl = await uploadImageToImgur(imageFile);
       if (uploadedImageUrl) {
-        setImages(uploadedImageUrl); // Assuming 'images' state stores the image URL
+        setImages(uploadedImageUrl);
       }
     }
   };
@@ -219,7 +201,9 @@ const Vendor = () => {
               onChange={(e) => setImageFile(e.target.files[0])}
             />
           </label>
-          <button onClick={redirectToImgurOAuth}>Authenticate with Imgur</button>
+          <button onClick={redirectToImgurOAuth}>
+            Authenticate with Imgur
+          </button>
           <button type="button" onClick={handleImageUpload}>
             Upload Image
           </button>
