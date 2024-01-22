0bce8b948ea64d489a5ba016684e6d5f9a5d6649
Vimaser <thfeske@gmail.com>
2024-01-22 15:44:16 -0600

    css header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaYoutubeSquare,
+  FaEnvelope,
+} from "react-icons/fa";
 import "./css/Header.css";
 // import "./css/darkMode.css";
+import logoImageUrl from "../img/logo.png";
 
 const Header = () => {
   const [user, setUser] = useState(null);
@@ -11,6 +18,20 @@ const Header = () => {
   // const [darkMode, setDarkMode] = useState(false);
   const location = useLocation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [showTopHeader, setShowTopHeader] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+    setShowTopHeader(currentScrollPos < 50); // Adjust 50 to the threshold you prefer
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   /*   const toggleDarkMode = () => {
     setDarkMode((prevMode) => {
@@ -61,12 +82,47 @@ const Header = () => {
   }, []); // Removed darkMode from dependency array
 
   return (
-    <div className="header-container">
-      <nav
-        className={`pure-menu pure-menu-horizontal ${
-          mobileMenuOpen ? "open" : ""
-        }`}
+    <div
+      className={
+        showTopHeader
+          ? "header-container"
+          : "header-container top-header-not-active"
+      }
+    >
+      {/* Logo and Social Media Links with Animation */}
+      <div
+        className={
+          showTopHeader ? "top-header top-header-active" : "top-header"
+        }
       >
+        <div className="logo-container">
+          <img src={logoImageUrl} alt="Logo" />
+        </div>
+        <div className="times">
+          {/* If the times are not meant to be a link, use a button */}
+         
+        </div>
+        <div className="social-media-links"> <p className="time">
+            Sat. 9am - 5pm | Sun. 9am - 5pm
+          </p><p1>   </p1>
+          <a href="https://www.facebook.com/FLEALA/">
+            <FaFacebookSquare />
+          </a><p1>  &#160;  </p1>
+          <a href="your_twitter_link">
+            <FaTwitterSquare />
+          </a><p1>  &#160;  </p1>
+          <a href="your_youtube_link">
+            <FaYoutubeSquare />
+          </a><p1>  &#160;  </p1>
+          <a href="mailto:fleamarket@eatel.net">
+            <FaEnvelope />
+          </a><p1>  &#160;  </p1>
+        </div>
+      </div>
+
+      {/* Navigation Menu */}
+      <nav className={`nav-bar ${mobileMenuOpen ? "open" : ""}`}>
+        {/* Hamburger Menu for Mobile */}
         <div className="hamburger" onClick={toggleMobileMenu}>
           <span></span>
           <span></span>
